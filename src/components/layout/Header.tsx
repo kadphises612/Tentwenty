a427ed1f017af3bb91cb27af35277f17b4a9c9e3
Abhishek <dev.abhi2902@gmail.com>
2026-06-04 00:32:57 +0530

    Added common header

---
+++ src/components/layout/Header.tsx
@@ -0,0 +1,24 @@
+import { ChevronDown } from 'lucide-react';
+
+export default function Header() {
+  return (
+    <header className="h-16 border-b border-slate-200 bg-white">
+      <div className="flex h-full items-center justify-between px-6">
+        <div className="flex items-center gap-10">
+          <h1 className="text-[24px] font-semibold tracking-tight text-gray-900">
+            ticktock
+          </h1>
+
+          <span className="text-[14px] font-medium  text-gray-900">
+            Timesheets
+          </span>
+        </div>
+
+        <button className="flex  items-center gap-1 text-md font-medium text-gray-500">
+          <span>John Doe</span>
+          <ChevronDown size={16} />
+        </button>
+      </div>
+    </header>
+  );
+}
